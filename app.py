841615779a6550bb15ac94fa2355bd8391ffc67d
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-05-31 10:30:51 -0400

    Rutas dinamicas y paridad

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ app = Flask(__name__)
 def inicio():
     return "Hola mundo desde mi primera API"
 
+
+#Saludo
 @app.route("/saludo")
 
 def saludo():
     return "Bienvenido a mi API"
 
+
+#Datos estaticos
 @app.route("/usuario")
 
 def usuario():
@@ -21,6 +25,29 @@ def usuario():
         "edad": 20,
         "carrera": "Ingenieria Informatica"
     })
+
+
+#Datos  por parametro
+@app.route("/saluda/<nombre>")
+
+def saludo_personalizado(nombre):
+    return jsonify({
+        "mensaje" : f"Hola {nombre}, bienvenido a mi API"
+    })
+
+
+@app.route("/paridad/<int:numero>")
+
+def paridad(numero): 
+    if numero%2==0:
+        resultado="Par"
+    else :
+        resultado= "Impar"
+    
+    return jsonify({
+        "numero": numero,
+        "resultado": resultado
+    })  
 
 if __name__ =="__main__":
     app.run(debug=True)
