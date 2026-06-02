d1b3d1167fa6d9067b179e6c3494e45c6723f9cd
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-06-01 20:13:35 -0400

    Ruta POST agregar usuario

--- app.py
+++ app.py
@@ -80,10 +80,30 @@ def buscar_usuario(numero):
     
     for i in  usuarios:
         if i["id"] == numero:
-            resultado = i 
             return jsonify(i)
           
     return jsonify({"error": "Usuario no encontrado"}), 404   
+
+@app.route("/nuevousuario", methods=["POST"])
+
+def nuevo_usuario():
+    datos = request.get_json()
+    nombre = datos["nombre"]
+    edad  = datos["edad"]
+    nuevoid = len(usuarios)+1
     
+    nuevo = {
+    "id": nuevoid,
+    "nombre": nombre,
+    "edad": edad
+    }
+    
+    usuarios.append(nuevo)
+    
+    return jsonify({
+        "Mensaje ": f"Usuario {nombre} creado exitosamente.",
+        "usuario ": nuevo
+    })
+
 if __name__ =="__main__":
     app.run(debug=True)
