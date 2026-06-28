06afed08e67de9be723f5bfe0da4138536befbc9
garecaarnildo8-cell <garecaarnildo8@gmail.com>
2026-06-28 18:17:00 -0400

    Add Flask CRUD API

---
+++ Repaso1.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+usuarios = []
+
+@app.route("/usuarios", methods=["GET"])
+def listar_usuarios():
+    return jsonify(usuarios)
+
+@app.route("/usuarios", methods=["POST"])
+def crear_usuario():
+    datos = request.get_json()
+    datos["id"] = len(usuarios)
+    usuarios.append(datos)
+    return jsonify({"Mensaje":"Creado exitosamente"}), 201
+
+@app.route("/usuarios/<id>", methods=["PUT"])
+def actualizar_usuario(id):
+    # tú escribes lo de adentro
+    
+    for i in usuarios:
+        if i["id"]==int(id):
+            datos = request.get_json()
+            i["nombre"] = datos["nombre"]
+            i["correo"] = datos["correo"]
+            
+            return jsonify({"Mensaje":"Usuario modificado exitosamente"}), 200
+@app.route("/usuarios/<id>", methods=["DELETE"])
+def eliminar_usuario(id):
+    for i in usuarios:
+        if i["id"]==int(id):
+            usuarios.remove(i)
+            return jsonify({"Mensaje":"Usuario eliminado exitosamente"}), 200
+if __name__ == "__main__":
+    app.run(debug=True)
